b04d76ac8f45a82d5a90d25e1982664161ef03e4
CoffeeTonya <127587138+CoffeeTonya@users.noreply.github.com>
2024-07-03 14:05:56 +0900

    Update point.py

--- point.py
+++ point.py
@@ -49,7 +49,7 @@ if selected_item == '受注設定':
     if 0 < product >= 1:
         st.write('※１行目にポイントが発生しない商品を入れないでください。')
         check01 = st.checkbox('ビーンズクラブ会員対象品', key=1001)
-        col03, col01, col01_1, col01_2, col02, col04 = st.beta_columns([2,2,2,2,2,2])
+        col03, col01, col01_1, col01_2, col02, col04 = st.columns([2,2,2,2,2,2])
         with col03:
             tax01 = st.selectbox('税率', [0.08, 0.1], key=3)
         with col01:
@@ -355,7 +355,7 @@ else:
     price = st.sidebar.text_input('税込金額', 0, key=1)
     tax = st.sidebar.selectbox('税率', [0.08, 0.1])
 
-    col01, col02 = st.beta_columns([1,1])
+    col01, col02 = st.columns([1,1])
     with col01:
         st.text_input('ホワイト会員（1%還元）', int((Decimal(price) / Decimal(1 + tax)) * Decimal(white)), key=2)
     with col02:
